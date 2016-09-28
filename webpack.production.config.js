aaabd3c172690bd619794d12d29c2b0f50dc3435
Sarah Mogin <mogin@work.co>
2016-09-27 22:02:32 -0400

    build, update prod webpack

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -6,21 +6,21 @@ var CopyWebpackPlugin = require('copy-webpack-plugin');
 module.exports = {
   devtool: 'cheap-source-map',
   entry: [
-    path.resolve(__dirname, 'app/main.jsx'),
+    path.resolve(__dirname, 'app/main.js'),
   ],
   output: {
-    path: __dirname + '/build',
+    path: __dirname + '/docs',
     publicPath: '/',
     filename: './bundle.js'
   },
   module: {
     loaders:[
-      { test: /\.css$/, include: path.resolve(__dirname, 'app'), loader: 'style-loader!css-loader' },
+      { test: /\.css$/, include: path.resolve(__dirname, 'app'), loader: 'style-loader!css-loader!postcss-loader' },
       { test: /\.js[x]?$/, include: path.resolve(__dirname, 'app'), exclude: /node_modules/, loader: 'babel-loader' },
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx'],
+    extensions: ['', '.js'],
   },
   plugins: [
     new webpack.optimize.DedupePlugin(),
@@ -33,5 +33,16 @@ module.exports = {
       { from: './app/index.html', to: 'index.html' },
       { from: './app/main.css', to: 'main.css' }
     ]),
-  ]
+  ],
+  postcss: function postcssInit(webpackInstance) {
+    return [
+      require('postcss-import')({
+      /* We need to assign postcss plugins inside this function
+         to pass webpackInstance to postcss-import. */
+        addDependencyTo: webpackInstance,
+      }),
+      require('postcss-cssnext'),
+      require('postcss-mixins'),
+    ];
+  },
 };
