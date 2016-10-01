6de281e6583aa5602224cecfc95281f2aed6e6b7
Sarah Mogin <mogin@work.co>
2016-09-30 21:46:54 -0400

    first three

--- app/components/circles/utils.js
+++ app/components/circles/utils.js
@@ -2,34 +2,34 @@ import R from 'ramda';
 
 import { OPERATIONS } from './index';
 
-export function buildRows(size) {
-  const rows = [];
+// export function buildRows(size) {
+//   const rows = [];
 
-  for (let i = size; i > 0; i--) {
-    rows.push(R.range(0, i).map(() => {
-      return {
-        statik: getStatic(),
-        dynamic: {
-          value: '',
-        },
-      };
-    }));
-  }
+//   for (let i = size; i > 0; i--) {
+//     rows.push(R.range(0, i).map(() => {
+//       return {
+//         statik: getStatic(),
+//         dynamic: {
+//           value: '',
+//         },
+//       };
+//     }));
+//   }
 
-  return rows;
-}
+//   return rows;
+// }
 
-function getStatic() {
-  const staticOdds = 4;
+// function getStatic() {
+//   const staticOdds = 4;
 
-  const out = {
-    bottomLeft: oneIn(staticOdds) ? getRandomPetalValue() : undefined,
-    bottomRight: oneIn(staticOdds) ? getRandomPetalValue() : undefined,
-    right: oneIn(staticOdds) ? getRandomPetalValue() : undefined,
-  };
+//   const out = {
+//     bottomLeft: oneIn(staticOdds) ? getRandomPetalValue() : undefined,
+//     bottomRight: oneIn(staticOdds) ? getRandomPetalValue() : undefined,
+//     right: oneIn(staticOdds) ? getRandomPetalValue() : undefined,
+//   };
 
-  return R.filter(R.identity, out);
-}
+//   return R.filter(R.identity, out);
+// }
 
 const nonPrimes = R.range(19, 82).filter(n =>
   R.any(x => {
@@ -39,13 +39,13 @@ const nonPrimes = R.range(19, 82).filter(n =>
 
 const PETAL_LIST = R.concat(R.range(1, 19), nonPrimes);
 
-function getRandomPetalValue() {
-  return getRandomFromList(PETAL_LIST);
-}
+// function getRandomPetalValue() {
+//   return getRandomFromList(PETAL_LIST);
+// }
 
-function oneIn(max = 2) {
-  return getRandom(max) === 1;
-}
+// function oneIn(max = 2) {
+//   return getRandom(max) === 1;
+// }
 
 export function getRandom(max = 10) {
   const list = R.range(1, max);
@@ -104,34 +104,58 @@ export const apply = (operation, ...args) => {
 
 function getRandomOperation(except = []) {
   const list = R.without(except, OPERATIONS);
-
   return list[getRandomFromList(R.keys(list))];
 }
 
-function getOperationAndStatic({ values, tried = [] }) {
+// const {
+//   operation: bottomLeft,
+//   statik: firstStatikBottomLeft,
+// } = getOperationAndStatic({ values: [secondValue, thirdValue] });
+
+// function setOperationAndStatic({ values, tried = [] }) {
+//   const operation = getRandomOperation(tried);
+//   const statik = operation.func(
+//     Math.max(values[0], values[1]),
+//     Math.min(values[0], values[1]),
+//   );
+
+//   if (statik % 1 === 0 && statik > 0) {
+//     return { statik, operation };
+//   }
+
+//   tried.push(operation);
+
+//   return getOperationAndStatic({ values, tried });
+// }
+
+function setBottomLeft({ circle, operations, neighbor, tried = [] }) {
   const operation = getRandomOperation(tried);
+  const values = [circle.getValue(), neighbor.getValue()];
+
   const statik = operation.func(
     Math.max(values[0], values[1]),
     Math.min(values[0], values[1]),
   );
 
   if (statik % 1 === 0 && statik > 0) {
-    return { statik, operation };
+    operations.bottomLeft = operation;
+    circle.setStatic({ bottomLeft: statik });
+    return false;
   }
 
   tried.push(operation);
 
-  return getOperationAndStatic({ values, tried });
+  return setBottomLeft({ circle, operations, neighbor, tried });
 }
 
-export function getValueAndStatiks(pairs) {
+export function setValueAndStatiks({ circle, operations, sets }) {
   const possibleStatiks = [];
   const possibleNewValues = R.range(1, 10);
 
-  pairs.forEach(({ value, operation }) => {
+  sets.forEach(({ setCircle, operationName }) => {
     possibleStatiks.push(
       possibleNewValues.map(newValue =>
-        apply(operation, newValue, value)
+        apply(operations[operationName], newValue, setCircle.getValue())
       )
     );
   });
@@ -142,132 +166,163 @@ export function getValueAndStatiks(pairs) {
     )
   );
 
-  if (valuesWithValidStatiks.length === 0) return {};
+  if (valuesWithValidStatiks.length === 0) return false;
 
   const value = getRandomFromList(valuesWithValidStatiks);
 
-  return {
-    value,
-    statiks: possibleStatiks.map(s => s[value - 1]),
-  };
+  circle.setValue(value);
+
+  sets.forEach(({ setCircle, operationName }, i) =>
+    setCircle.setStatic({ [operationName]: possibleStatiks[i][value - 1] })
+  );
+
+  return true;
 }
 
-export function setUpGame() {
-  const firstValue = getRandom();
-
-  const right = getRandomOperation();
-  const bottomRight = getRandomOperation();
-
-  const {
-    value: secondValue,
-    statiks: [
-      firstStatikRight,
-    ],
-  } = getValueAndStatiks([
-    { value: firstValue, operation: right },
-  ]);
-
-  const {
-    value: fourthValue,
-    statiks: [
-      firstStatikBottomRight,
-    ],
-  } = getValueAndStatiks([
-    { value: firstValue, operation: bottomRight },
-  ]);
-
-  const {
-    operation: bottomLeft,
-    statik: firstStatikBottomLeft,
-  } = getOperationAndStatic({ values: [secondValue, fourthValue] });
-
-  const {
-    value: thirdValue,
-    statiks: [
-      secondStatikRight,
-    ],
-  } = getValueAndStatiks([
-    { value: secondValue, operation: right },
-  ]);
-
-  const {
-    value: fifthValue,
-    statiks: [
-      secondStatikBottomRight,
-      secondStatikBottomLeft,
-      thirdStatikRight,
-    ],
-  } = getValueAndStatiks([
-    { value: secondValue, operation: bottomRight },
-    { value: thirdValue, operation: bottomLeft },
-    { value: fourthValue, operation: right },
-  ]);
-
-  const {
-    value: sixthValue,
-    statiks: [
-      thirdStatikBottomRight,
-      thirdStatikBottomLeft,
-    ],
-  } = getValueAndStatiks([
-    { value: fourthValue, operation: bottomRight },
-    { value: fifthValue, operation: bottomLeft },
-  ]);
-
-  if (!fifthValue || !sixthValue) {
-    /* eslint-disable no-console */
-    console.log('didnt work, starting over');
-    /* eslint-enable no-console */
-    return setUpGame();
+class Circle {
+  constructor(statik = {}) {
+    this.statik = statik;
   }
 
-  return {
-    rows: [
-      [
-        {
-          dynamic: {},
-          statik: {
-            bottomRight: firstStatikBottomRight,
-            right: firstStatikRight,
-          },
-        },
-        {
-          dynamic: {},
-          statik: {
-            bottomLeft: firstStatikBottomLeft,
-            bottomRight: secondStatikBottomRight,
-            right: secondStatikRight,
-          },
-        },
-        {
-          dynamic: {},
-          statik: {
-            bottomLeft: secondStatikBottomLeft,
-          },
-        },
-      ],
-      [
-        {
-          dynamic: {},
-          statik: {
-            bottomRight: thirdStatikBottomRight,
-            right: thirdStatikRight,
-          },
-        },
-        {
-          dynamic: {},
-          statik: {
-            bottomLeft: thirdStatikBottomLeft,
-          },
-        },
-      ],
-      [
-        {
-          dynamic: {},
-          statik: {},
-        },
-      ],
+  setValue(val) {
+    this.value = val;
+  }
+
+  getValue() {
+    return this.value;
+  }
+
+  setStatic(statikPair) {
+    this.statik = R.merge(this.statik, statikPair);
+  }
+
+  toObj() {
+    return {
+      statik: this.statik,
+      dynamic: {},
+    };
+  }
+}
+
+function buildRows(rows, rowCount) {
+  if (rowCount === 0) return rows;
+
+  const row = [];
+  R.times(() => {
+    row.push(new Circle());
+  }, rowCount);
+  rows.push(row);
+
+  return buildRows(rows, rowCount - 1);
+}
+
+export function setUpGame(size = 2) {
+  /* eslint-disable no-console */
+  console.log('setting up game');
+  /* eslint-enable no-console */
+
+  const operations = {
+    right: getRandomOperation(),
+    bottomRight: getRandomOperation(),
+  };
+
+  const rows = buildRows([], size);
+
+  rows[0][0].setValue(getRandom());
+
+  setValueAndStatiks({
+    circle: rows[0][1],
+    operations,
+    sets: [
+      { setCircle: rows[0][0], operationName: 'right' },
     ],
+  }) || setUpGame();
+
+  setValueAndStatiks({
+    circle: rows[1][0],
+    operations,
+    sets: [
+      { setCircle: rows[0][0], operationName: 'bottomRight' },
+    ],
+  }) || setUpGame();
+
+  setBottomLeft({
+    circle: rows[0][1],
+    neighbor: rows[1][0],
+    operations,
+  });
+
+  // console.log(rows);
+
+  // const notAllRowsHaveValues = rows.some(row =>
+  //   row.some(circle => circle.getValue());
+  // );
+
+  // if (notAllRowsHaveValues) {
+  //   /* eslint-disable no-console */
+  //   console.log('didnt work, starting over');
+  //   /* eslint-enable no-console */
+  //   return setUpGame();
+  // }
+
+  // const {
+  //   value: thirdValue,
+  //   statiks: [
+  //     firstStatikBottomRight,
+  //   ],
+  // } = getValueAndStatiks([
+  //   { value: firstValue, operation: bottomRight },
+  // ]);
+
+  // const {
+  //   operation: bottomLeft,
+  //   statik: firstStatikBottomLeft,
+  // } = getOperationAndStatic({ values: [secondValue, thirdValue] });
+
+  // const {
+  //   value: thirdValue,
+  //   statiks: [
+  //     secondStatikRight,
+  //   ],
+  // } = getValueAndStatiks([
+  //   { value: secondValue, operation: right },
+  // ]);
+
+  // const {
+  //   value: fifthValue,
+  //   statiks: [
+  //     secondStatikBottomRight,
+  //     secondStatikBottomLeft,
+  //     thirdStatikRight,
+  //   ],
+  // } = getValueAndStatiks([
+  //   { value: secondValue, operation: bottomRight },
+  //   { value: thirdValue, operation: bottomLeft },
+  //   { value: fourthValue, operation: right },
+  // ]);
+
+  // const {
+  //   value: sixthValue,
+  //   statiks: [
+  //     thirdStatikBottomRight,
+  //     thirdStatikBottomLeft,
+  //   ],
+  // } = getValueAndStatiks([
+  //   { value: fourthValue, operation: bottomRight },
+  //   { value: fifthValue, operation: bottomLeft },
+  // ]);
+
+  // if (!fifthValue || !sixthValue) {
+  //   /* eslint-disable no-console */
+  //   console.log('didnt work, starting over');
+  //   /* eslint-enable no-console */
+  //   return setUpGame();
+  // }
+
+  return {
+    rows: rows.map(row =>
+      row.map(circle => circle.toObj())
+    ),
     operations: {
       right: OPERATIONS.ADD,
       bottomLeft: OPERATIONS.ADD,
